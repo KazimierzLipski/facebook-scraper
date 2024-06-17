e4e9de15797ed89612ae8853d9927df4c58eba1e
Kazimierz Lipski <kazimierz.lipski1@gmail.com>
2024-06-17 13:04:50 +0200

    removed usage of lxml

--- facebook_scraper/utils.py
+++ facebook_scraper/utils.py
@@ -6,7 +6,6 @@ from typing import Optional
 from urllib.parse import parse_qsl, unquote, urlencode, urljoin, urlparse, urlunparse
 
 import dateparser
-import lxml.html
 from bs4 import BeautifulSoup
 from requests.cookies import RequestsCookieJar
 from requests_html import DEFAULT_URL, Element, PyQuery
@@ -209,7 +208,7 @@ def parse_datetime(text: str, search=True) -> Optional[datetime]:
 
 
 def html_element_to_string(element: Element, pretty=False) -> str:
-    html = lxml.html.tostring(element.element, encoding='unicode')
+    html = BeautifulSoup(element.html, features='html.parser').prettify()
     if pretty:
         html = BeautifulSoup(html, features='html.parser').prettify()
     return html
